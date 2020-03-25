05737b0b9b7994ac74eccc4df5c5f98240d125ef
antsy <antsyandres@gmail.com>
2020-03-25 13:37:29 +0800

    login page quick change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-12 col-form-label">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-12 col-form-label offset-md-1">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-10 offset-md-1">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -37,7 +37,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-12 col-form-label offset-md-1">{{ __('Password') }}</label>
 
                             <div class="col-md-10 offset-md-1">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-8 offset-md-3">
+                            <div class="col-md-8 offset-md-1">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -63,14 +63,14 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-3">
+                            <div class="col-md-10 offset-md-1">
                                 <button type="submit" class="btn btn-outline-success float-right">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Reset Password') }}
                                     </a>
                                 @endif
                             </div>
